285b0b10cb0a74007b8ba4f6bfdf36c18c324150
Alena <elen.malets@gmail.com>
2019-05-04 10:51:21 +0200

    async done

---
+++ src/async-functions.js
@@ -0,0 +1,30 @@
+function giveItBackLater(value, callback) {
+    function doSmth() {
+        callback(value)
+    }
+    setTimeout(doSmth, 100)
+}
+
+function addSomePromises(somePromise) {
+    return new Promise(function (resolve) {
+        setTimeout(() => resolve(somePromise), 100);
+    }).then(function (result) {
+        return result.repeat(2)
+    }).catch(function (result) {
+        return result.repeat(3)
+    })
+}
+
+function promiseToGiveItBackLater(value) {
+    return new Promise((resolve) => {
+        function doSmth() {
+            resolve(value)
+        }
+        setTimeout(doSmth, 100)
+    })
+}
+
+
+module.exports.giveItBackLater = giveItBackLater
+module.exports.addSomePromises = addSomePromises
+module.exports.promiseToGiveItBackLater = promiseToGiveItBackLater
